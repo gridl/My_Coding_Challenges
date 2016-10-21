f2832a74f402d38f809c7b5ab68081787ff1636d
natenka <nataliya.samoylenko@gmail.com>
2016-10-21 15:17:15 +0300

    Add exercises chapter 3

--- pythonds/chapter3_basic_DS/prefix_postfix.py
+++ pythonds/chapter3_basic_DS/prefix_postfix.py
@@ -2,15 +2,23 @@ from stack_implementation import Stack
 import string
 import operator
 
+precedence = { '^': 4,
+               '*': 3,
+               '/': 3,
+               '+': 2,
+               '-': 2,
+               '(': 1 }
+
+operators = { '*': operator.mul,
+              '/': operator.truediv,
+              '+': operator.add,
+              '-': operator.sub,
+              '^': operator.pow }
+
+
 def infix_to_postfix(infix_exp):
     operators = Stack()
     infix_exp = infix_exp.split()
-    precedence = { '^': 4,
-                   '*': 3,
-                   '/': 3,
-                   '+': 2,
-                   '-': 2,
-                   '(': 1 }
 
     postfix = []
     for sym in infix_exp:
@@ -36,11 +44,6 @@ def infix_to_postfix(infix_exp):
 def eval_postfix(postfix_exp):
     operands = Stack()
     postfix = postfix_exp.split()
-    operators = { '*': operator.mul,
-                  '/': operator.truediv,
-                  '+': operator.add,
-                  '-': operator.sub,
-                  '^': operator.pow }
 
     for sym in postfix:
         if sym.isdigit():
@@ -54,6 +57,38 @@ def eval_postfix(postfix_exp):
     return operands.pop()
 
 
+def eval_infix(infix_exp):
+    operands = Stack()
+    op_stack = Stack()
+    infix = infix_exp.split()
+
+    for sym in infix:
+        if sym.isdigit():
+            operands.push(sym)
+        elif sym == '(':
+            op_stack.push(sym)
+        elif sym == ')':
+            second = float(operands.pop())
+            first = float(operands.pop())
+            op = op_stack.pop()
+            operands.push( operators[op](first, second) )
+            op_stack.pop()
+        elif sym in operators.keys():
+            if not op_stack.isEmpty() and precedence[op_stack.peek()] >= precedence[sym]:
+                op = op_stack.pop()
+                second = float(operands.pop())
+                first = float(operands.pop())
+                operands.push( operators[op](first, second) )
+            op_stack.push(sym)
+
+    while not operands.size() == 1:
+        second = float(operands.pop())
+        first = float(operands.pop())
+        op = op_stack.pop()
+        operands.push( operators[op](first, second) )
+    return operands.pop()
+
+
 if __name__ == "__main__":
     print("Convert infix to postfix:")
     print("A * B + C * D", '\t\t', infix_to_postfix("A * B + C * D"))
@@ -63,3 +98,6 @@ if __name__ == "__main__":
     print("Eval postfix: ")
     print(eval_postfix('17 8 + 3 2 + /'))
     print(eval_postfix('5 3 4 2 - ^ *'))
+
+    print("Eval infix: ")
+    print( eval_infix("( 9 / 3 ) ^ ( 1 + 2 )") )
